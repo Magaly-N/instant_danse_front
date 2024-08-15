ea912e4418a0e135531a5e9c45ddc16b9ca41a04
Magaly-N <magalynou@hotmail.fr>
2024-08-15 20:12:16 +0200

    Modif logo

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -5,7 +5,7 @@ const Logo = () => {
     return (
         <Link to="/">
             <source srcSet="img\logo.webp" className=" logo logo-webp" type="image/webp" alt="Dessin d'une femme en mouvement" title="Dessin d'une femme en mouvement" />
-            <img src="img\logo.jpg" className="logo logo-png" alt="Dessin d'une femme en mouvement" title="Dessin d'une femme en mouvement" />
+            <img src="img\logo.png" className="logo logo-png" alt="Dessin d'une femme en mouvement" title="Dessin d'une femme en mouvement" />
         </Link>
     )
 }
